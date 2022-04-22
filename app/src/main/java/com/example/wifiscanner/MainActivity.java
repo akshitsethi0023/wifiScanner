807d554f28f9e8249613337903c268e953845fdd
Akshit Sethi <asethi0023@gmail.com>
2022-04-23 01:24:49 +0530

    request for camera access created.

--- app/src/main/java/com/example/wifiscanner/MainActivity.java
+++ app/src/main/java/com/example/wifiscanner/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.wifiscanner;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -8,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -17,18 +23,58 @@ import com.example.wifiscanner.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
-
+    private static final int REQUEST_CAMERA_ID = 101;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.cameraImage);
+        checkAndRequestPermissions(MainActivity.this);
+    }
+    public static boolean checkAndRequestPermissions(final Activity context) {
+        int cameraPermission = ContextCompat.checkSelfPermission(context,
+                Manifest.permission.CAMERA);
 
+        if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(context, new String[] {Manifest.permission.CAMERA},
+                    REQUEST_CAMERA_ID);
+            return false;
+        }
 
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_CAMERA_ID:
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(),
+                            "ALERT: Access to Camera required.", Toast.LENGTH_LONG)
+                            .show();
+                } else {
+                    saveImage(MainActivity.this);
+                }
+                break;
+        }
+    }
+
+    public void saveImage(Context context){
 
     }
+
 }
